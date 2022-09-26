dc7361ce373767683bfeac766349dfe1ac9811ad
Khairunisa701210118 <114423606+Khairunisa701210118@users.noreply.github.com>
2022-09-26 22:36:43 +0700

    Add files via upload

---
+++ latihan4d.php
@@ -0,0 +1,64 @@
+<?php
+
+$nilai = array (
+    array("negara","Indonesia","singapura","malaysia","brunei","tailand","laos","filipina","myanmar"),
+    array("ibu kota","D.K.I. jakarta","singapura","kuala lumpur","bandar sri begawan","bangkok","vientiane","manila","naypyidaw"),
+    array("kode telepon","+62","+65","+60","+673","+66","+856","+63","+95")
+);
+?>
+
+<html>
+    <head>
+        <title>LATIHAN 4D</title>
+    </head>
+    <body>
+        
+            <table border="1" cellpadding="5">
+                <tr>
+                    <th><?php echo $nilai[0][0]?></th>
+                    <th><?php echo $nilai[1][0]?></th>
+                    <th><?php echo $nilai[2][0]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][1]?></th>
+                    <th><?php echo $nilai[1][1]?></th>
+                    <th><?php echo $nilai[2][1]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][2]?></th>
+                    <th><?php echo $nilai[1][2]?></th>
+                    <th><?php echo $nilai[2][2]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][3]?></th>
+                    <th><?php echo $nilai[1][3]?></th>
+                    <th><?php echo $nilai[2][3]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][4]?></th>
+                    <th><?php echo $nilai[1][4]?></th>
+                    <th><?php echo $nilai[2][4]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][5]?></th>
+                    <th><?php echo $nilai[1][5]?></th>
+                    <th><?php echo $nilai[2][5]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][6]?></th>
+                    <th><?php echo $nilai[1][6]?></th>
+                    <th><?php echo $nilai[2][6]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][7]?></th>
+                    <th><?php echo $nilai[1][7]?></th>
+                    <th><?php echo $nilai[2][7]?></th>
+                </tr>
+                <tr>
+                    <th><?php echo $nilai[0][8]?></th>
+                    <th><?php echo $nilai[1][8]?></th>
+                    <th><?php echo $nilai[2][8]?></th>
+                </tr>
+            </table>
+    </body>
+</html>
